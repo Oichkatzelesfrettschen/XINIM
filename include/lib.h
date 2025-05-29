aa90812b35c7b5263e30c7e9e1d4aad744f43e65
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-29 13:02:30 -0700

    Update prototypes and signal handlers

--- include/lib.h
+++ include/lib.h
@@ -2,12 +2,16 @@
 #include "../h/type.h"
 #include "../h/error.h"
 #include "../h/callnr.h"
+#include <stddef.h>
+#include <stdint.h>
 
 extern message M;
 
 #define MM                 0
 #define FS                 1
-
-extern int callm1(), callm3(), callx(), len();
+extern int callm1(int proc, int syscallnr, int int1, int int2, int int3, char *ptr1, char *ptr2, char *ptr3);
+extern int callm3(int proc, int syscallnr, int int1, char *name);
+extern int callx(int proc, int syscallnr);
+extern int len(char *s);
 extern int errno;
-extern int begsig();		/* interrupts all vector here */
+extern int begsig(void);            /* interrupts all vector here */
