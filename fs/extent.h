a50930f997df51ca2456e847cbfa5bdab40ac7dd
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-29 01:46:18 -0700

    fs: add 64-bit file size and extent support

---
+++ fs/extent.h
@@ -0,0 +1,15 @@
+/* Extent-based allocation structures. */
+#ifndef FS_EXTENT_H
+#define FS_EXTENT_H
+
+#include "../h/type.h"
+
+/* An extent describes a contiguous range of zones on disk. */
+typedef struct {
+  zone_nr e_start; /* first zone in the extent */
+  zone_nr e_count; /* number of zones in the extent */
+} extent;
+
+#define NIL_EXTENT (extent *)0
+
+#endif /* FS_EXTENT_H */
