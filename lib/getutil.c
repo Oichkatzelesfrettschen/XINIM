a48503d2beb6b9b621df4a644da04db8a9847ae5
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-29 05:08:23 -0700

    fix pwd field names

--- lib/getutil.c
+++ lib/getutil.c
@@ -1,4 +1,3 @@
-#include "../h/type.h"
 #include "../include/lib.h"
 
 extern char endbss; /* Symbol marking end of BSS provided by end.s */
@@ -6,23 +5,14 @@ extern char endbss; /* Symbol marking end of BSS provided by end.s */
 /*
  * Return the base memory click.  The original assembly stub returned zero.
  */
-PUBLIC phys_clicks get_base(void)
-{
-    return 0;
-}
+PUBLIC phys_clicks get_base(void) { return 0; }
 
 /*
  * Return the address of the end of the BSS segment.
  */
-PUBLIC phys_clicks get_size(void)
-{
-    return (phys_clicks)&endbss;
-}
+PUBLIC phys_clicks get_size(void) { return (phys_clicks)&endbss; }
 
 /*
  * Return the total amount of memory available.
  */
-PUBLIC phys_clicks get_tot_mem(void)
-{
-    return 0;
-}
+PUBLIC phys_clicks get_tot_mem(void) { return 0; }
