21bc03576c3e87d2e1b8fcb4fb759ea7af91a1bc
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-29 21:10:11 -0700

    Fix headers and syscalls

--- kernel/type.h
+++ kernel/type.h
@@ -1,3 +1,6 @@
+#ifndef KERNEL_TYPE_H
+#define KERNEL_TYPE_H
+
 /* The 'pc_psw' struct is machine dependent.  It must contain the information
  * pushed onto the stack by an interrupt, in the same format as the hardware
  * creates and expects.  It is used for storing the interrupt status after a
@@ -6,11 +9,13 @@
 
 #include <stdint.h>
 struct pc_psw {
-  uint64_t rip;                 /* instruction pointer */
-  uint64_t rflags;              /* rflags register */
+    uint64_t pc;  /* program counter */
+    uint64_t psw; /* processor status word */
 };
 
 struct sig_info {
-  int signo;
-  struct pc_psw sigpcpsw;
+    int signo;
+    struct pc_psw sigpcpsw;
 };
+
+#endif /* KERNEL_TYPE_H */
--- lib/syscall_x86_64.c
+++ lib/syscall_x86_64.c
@@ -1,35 +1,41 @@
 #ifdef __x86_64__
 #include "../h/com.h"
+#include "../h/const.h"
 #include "../h/type.h"
 
-PUBLIC int send(int dst, message *m_ptr)
-{
-    register long rax asm("rax") = 0;
-    register long rdi asm("rdi") = dst;
-    register message *rsi asm("rsi") = m_ptr;
-    register long rdx asm("rdx") = SEND;
-    asm volatile ("syscall" : "=a"(rax) : "D"(rdi), "S"(rsi), "d"(rdx), "a"(rax) : "rcx", "r11", "memory");
+PUBLIC int send(int dst, message *m_ptr) {
+    register long rax __asm__("rax") = 0;
+    register long rdi __asm__("rdi") = dst;
+    register message *rsi __asm__("rsi") = m_ptr;
+    register long rdx __asm__("rdx") = SEND;
+    __asm__ volatile("syscall"
+                     : "=a"(rax)
+                     : "D"(rdi), "S"(rsi), "d"(rdx), "a"(rax)
+                     : "rcx", "r11", "memory");
     return (int)rax;
 }
 
-PUBLIC int receive(int src, message *m_ptr)
-{
-    register long rax asm("rax") = 0;
-    register long rdi asm("rdi") = src;
-    register message *rsi asm("rsi") = m_ptr;
-    register long rdx asm("rdx") = RECEIVE;
-    asm volatile ("syscall" : "=a"(rax) : "D"(rdi), "S"(rsi), "d"(rdx), "a"(rax) : "rcx", "r11", "memory");
+PUBLIC int receive(int src, message *m_ptr) {
+    register long rax __asm__("rax") = 0;
+    register long rdi __asm__("rdi") = src;
+    register message *rsi __asm__("rsi") = m_ptr;
+    register long rdx __asm__("rdx") = RECEIVE;
+    __asm__ volatile("syscall"
+                     : "=a"(rax)
+                     : "D"(rdi), "S"(rsi), "d"(rdx), "a"(rax)
+                     : "rcx", "r11", "memory");
     return (int)rax;
 }
 
-PUBLIC int sendrec(int srcdest, message *m_ptr)
-{
-    register long rax asm("rax") = 0;
-    register long rdi asm("rdi") = srcdest;
-    register message *rsi asm("rsi") = m_ptr;
-    register long rdx asm("rdx") = BOTH;
-    asm volatile ("syscall" : "=a"(rax) : "D"(rdi), "S"(rsi), "d"(rdx), "a"(rax) : "rcx", "r11", "memory");
+PUBLIC int sendrec(int srcdest, message *m_ptr) {
+    register long rax __asm__("rax") = 0;
+    register long rdi __asm__("rdi") = srcdest;
+    register message *rsi __asm__("rsi") = m_ptr;
+    register long rdx __asm__("rdx") = BOTH;
+    __asm__ volatile("syscall"
+                     : "=a"(rax)
+                     : "D"(rdi), "S"(rsi), "d"(rdx), "a"(rax)
+                     : "rcx", "r11", "memory");
     return (int)rax;
 }
 #endif
-
