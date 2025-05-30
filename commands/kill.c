4370035912a87b072a009bfcaa2818a7d6a0eebe
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-30 05:52:56 -0700

    Refactor command utilities to modern C++ style

--- commands/kill.c
+++ commands/kill.c
@@ -2,34 +2,32 @@
 
 #include "../h/signal.h"
 
-main(argc,argv)
-int argc;
-char **argv;
-{
-  int proc, signal = SIGTERM;
+int main(int argc, char **argv) {
+    int proc, signal = SIGTERM;
 
-  if (argc < 2) usage();
-  if (*argv[1] == '-') {
-	signal = atoi(&argv[1][1]);
-	*argv++;
-	argc--;
-  }
-  if (!signal) signal = SIGTERM;
-  while(--argc) {
-	*argv++;
-	proc = atoi(*argv);
-	if (!proc && strcmp(*argv, "0"))
-		usage();
-	if (kill(proc,signal)) {
-		prints("Kill: %s no such process\n", itoa(proc));
-		exit(1);
-	}
-  }
-  exit(0);
+    if (argc < 2)
+        usage();
+    if (*argv[1] == '-') {
+        signal = atoi(&argv[1][1]);
+        *argv++;
+        argc--;
+    }
+    if (!signal)
+        signal = SIGTERM;
+    while (--argc) {
+        *argv++;
+        proc = atoi(*argv);
+        if (!proc && strcmp(*argv, "0"))
+            usage();
+        if (kill(proc, signal)) {
+            prints("Kill: %s no such process\n", itoa(proc));
+            exit(1);
+        }
+    }
+    exit(0);
 }
 
-usage()
-{
-  prints("usage: kill pid\n");
-  exit(1);
+static void usage() {
+    prints("usage: kill pid\n");
+    exit(1);
 }
