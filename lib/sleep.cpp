4a4e5d218edcdca6726f2e918c5be1f2b1dff5e1
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-30 14:07:57 -0700

    Replace macro constants with constexpr

--- lib/sleep.cpp
+++ lib/sleep.cpp
@@ -1,8 +1,8 @@
 #include "../include/lib.hpp" // C++17 header
 #include "../include/signal.h"
 
-PRIVATE void alfun(int signum) { (void)signum; }
-PUBLIC void sleep(int n) {
+static void alfun(int signum) { (void)signum; }
+void sleep(int n) {
     /* sleep(n) pauses for 'n' seconds by scheduling an alarm interrupt. */
     signal(SIGALRM, alfun);
     alarm(n);
