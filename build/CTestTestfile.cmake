0e54e6b2a9471711ff5b8ed7f9dbb05663e7d230
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-06-28 00:39:33 -0700

    test
    test

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /Users/eirikr/Documents/GitHub/minix1
+# Build directory: /Users/eirikr/Documents/GitHub/minix1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("lib")
+subdirs("kernel")
+subdirs("mm")
+subdirs("fs")
+subdirs("commands")
+subdirs("tools")
+subdirs("tests")
